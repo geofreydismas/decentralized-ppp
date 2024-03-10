cfd6e003eb794e60d3e4eef092f1623465f712d5
geofreydismas <geoffreydismas@hotmail.com>
2024-03-11 00:08:01 +0300

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 "# decentralized-ppp" 
+"# my_list.ppp" 
---
+++ pythonvar.py
@@ -0,0 +1,31 @@
+site_name = "DARZOO"
+print(site_name)
+a,b,c=5,6,'hello'
+print(a)
+print(b)
+print(c)
+num=5
+print(num,'is of type', type(num))
+languages=['burundi','congo','USA']
+print(languages[0])
+print(languages[2])
+A=4
+B=7
+print(A>B)
+print('good morning !', end=' ')
+print('its rainy day')
+my_list=[1, 'hello', 5.6]
+print(my_list[0])
+listd=['p','r','o','g','r','a','m']
+listd.append(32)
+print(listd)
+print(listd[2:5])
+print(listd[:])
+for listg in listd:
+    print(listg)
+capital_city={"japan": 2,"kenya": 4, "tanzania": 9}
+print(capital_city)
+capital_city["tokyo"]="pwani"
+print(capital_city)
+del capital_city ["kenya"]
+print(capital_city)
